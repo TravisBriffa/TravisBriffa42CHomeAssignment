b82678ec7fc2d6b5baf0513cc81fbdb5d48824a7
TravisBriffa <jloleris@gmail.com>
2021-01-07 18:27:51 +0100

    uploaded sounds, background which scrolls has been created

---
+++ 42CHomeAssignment/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.02f;
+    Material roadBackground;
+    Vector2 offset;
+
+    void Start()
+    {
+        roadBackground = GetComponent<Renderer>().material;
+        offset = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+
+    void Update()
+    {
+        roadBackground.mainTextureOffset += offset * Time.deltaTime;
+
+    }
+}
